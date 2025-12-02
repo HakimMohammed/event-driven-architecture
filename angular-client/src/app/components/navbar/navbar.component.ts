a49b3e3ed72ac88b6dfeae7dbef8193947a34d60
HakimMohammed <mohamed.hakim.dev@gmail.com>
2025-12-02 19:36:55 +0100

    Angular Banking App v2

--- angular-client/src/app/components/navbar/navbar.component.ts
+++ angular-client/src/app/components/navbar/navbar.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
+import { ThemeService } from '../../services/theme.service';
 
 @Component({
     selector: 'app-navbar',
@@ -9,4 +10,6 @@ import { RouterModule } from '@angular/router';
     templateUrl: './navbar.component.html',
     styleUrls: ['./navbar.component.css']
 })
-export class NavbarComponent { }
+export class NavbarComponent {
+    constructor(public themeService: ThemeService) { }
+}
